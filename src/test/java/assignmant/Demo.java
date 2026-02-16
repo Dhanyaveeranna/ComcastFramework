b2adc66a33ab7cb7eeab5cc98624cd727f6cf71e
HP <HP@DESKTOP-JAJPE5G>
2026-02-16 17:06:48 +0530

    pushing to loacl 1branch

--- src/test/java/assignmant/Demo.java
+++ src/test/java/assignmant/Demo.java
@@ -4,7 +4,8 @@ import org.testng.reporters.jq.Main;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hai");
+		System.out.println("Bye");
 	}
 
 }
